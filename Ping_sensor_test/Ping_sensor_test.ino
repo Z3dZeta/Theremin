2915324ca3e8745163cd5f91557305966d44ba33
Z3dZeta <mmellema3@gmail.com>
2025-10-18 16:03:14 +0200

    Start implementatie state machine

--- Ping_sensor_test/Ping_sensor_test.ino
+++ Ping_sensor_test/Ping_sensor_test.ino
@@ -1,13 +1,29 @@
 //D7 = Echo pin > PD7 > Input
 //D8 = Trig pin > PB0 > Output
 
-  DDRC &= ~(1 << PD7);  //Echo pin als Input
-  PORTC |= (1 << PD7);  //Echo pin pull-up High
+DDRC &= ~(1 << PD7);  //Echo pin als Input
+PORTC |= (1 << PD7);  //Echo pin pull-up High
 
-  DDRC |= (1 << PB0); //Trig pin als output
+DDRC |= (1 << PB0);  //Trig pin als output
 
-  while (1){
-    
+typedef enum {
+  trig_on,
+  trig_off,
+  echo_on,
+}
+
+State currentState = trig_off;
+
+while (1) {
+
+  switch (currentState){
+    case trig_on:
+      PINB |= (1 << PB6);
+      currentState = trig_off;
   }
 
-
+    case trig_off:
+      if ((1 << PB5) == 1){
+        currentState = echo_off;
+      }
+}
